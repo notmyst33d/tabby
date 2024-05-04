5e26a173dc96dda196a11371dc7fcebf1425712b
Myst33d <notmyst33d@gmail.com>
2024-05-04 12:30:23 +0600

    Update

--- tabby_api.py
+++ tabby_api.py
@@ -1,8 +1,30 @@
+import os
 import json
-from llama_cpp import Llama
 from aiohttp.web import Application, RouteTableDef, Request, StreamResponse, json_response, run_app
 
+backend = os.environ.get("BACKEND", "llamacpp")
+hf_model = os.environ.get("HFMODEL")
+hf_token = os.environ.get("HFTOKEN")
 routes = RouteTableDef()
+
+class HuggingfaceAdapter:
+    def __init__(self, client):
+        self.client = client
+
+    def create_chat_completion(self, *args, **kwargs):
+        if kwargs.get("top_k"):
+            del kwargs["top_k"]
+        kwargs["max_tokens"] = 16384
+        if kwargs.get("stream", False):
+            for completion in self.client.chat_completion(*args, **kwargs):
+                if not completion.choices:
+                    continue
+                yield {"choices": [{"delta": {"content": completion.choices[0].delta.content}, "finish_reason": completion.choices[0].finish_reason}]}
+        else:
+            completion = self.client.chat_completion(*args, **kwargs)
+            print(completion)
+            yield {"choices": [{"delta": {"content": completion.choices[0].message.content}, "finish_reason": None}]}
+            yield {"choices": [{"delta": {"content": None}, "finish_reason": completion.choices[0].finish_reason}]}
 
 @routes.post("/prompt")
 async def post_prompt(request: Request):
@@ -31,22 +53,30 @@ async def post_prompt(request: Request):
     )
 
     for completion in stream:
+        print(completion)
         await response.write(json.dumps({
             "token": completion["choices"][0]["delta"].get("content", ""),
-            "stop": completion["choices"][0]["finish_reason"] == "stop",
+            "stop": completion["choices"][0]["finish_reason"] != None,
         }).encode("utf-8") + b"\n")
 
     await response.write_eof()
 
 app = Application()
-app["llm"] = Llama(
-    model_path="models/Meta-Llama-3-8B-Instruct-Q4_K_M.gguf",
-    use_mmap=False,
-    use_mlock=True,
-    n_threads=4,
-    n_ctx=512,
-    flash_attn=True,
-    verbose=True,
-)
+if backend == "huggingface":
+    from huggingface_hub import InferenceClient
+    client = InferenceClient(model=hf_model, token=hf_token)
+    app["llm"] = HuggingfaceAdapter(client)
+elif backend == "llamacpp":
+    from llama_cpp import Llama
+    app["llm"] = Llama(
+        model_path="models/Meta-Llama-3-8B-Instruct-Q4_K_M.gguf",
+        use_mmap=False,
+        use_mlock=True,
+        n_threads=4,
+        n_ctx=512,
+        flash_attn=True,
+        verbose=True,
+    )
+
 app.add_routes(routes)
 run_app(app, port=10000)
